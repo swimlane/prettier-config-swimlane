bc6d34c5492b44a364d2aba17a5e71cd07beb00b
Alex Bainter <alexbainter@gmail.com>
2018-04-16 12:22:54 -0500

    Add test for valid prettier configuration

--- test/lib/prettier.config.spec.js
+++ test/lib/prettier.config.spec.js
@@ -1,8 +1,10 @@
+const { validate } = require('jsonschema');
 const configSchema = require('./schema');
+const prettierConfig = require('../../lib/prettier.config');
 
 describe('Prettier configuration', () => {
   it('should match the prettier schema', () => {
-    console.log(configSchema);
-    expect(true).toBe(false);
+    const { valid } = validate(prettierConfig, configSchema);
+    expect(valid).toBe(true, 'Configuration does not match schema.');
   });
 });
